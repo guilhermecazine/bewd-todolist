71359554829ca3cb1f0c6469858e76af0d5b59f1
guilhermecazine <guilhermecazine50@gmail.com>
2024-07-12 07:13:27 +0100

    Fixed routes and adjusted tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,47 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      render :create, status: :created
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find_by(id: params[:id])
+    if @task&.destroy
+      render json: { success: true }
+    else
+      render json: { success: false }, status: :not_found
+    end
+  end
+
+  def mark_complete
+    @task = Task.find_by(id: params[:id])
+    if @task&.update(completed: true)
+      render :mark_complete
+    else
+      render json: { errors: @task.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def mark_active
+    @task = Task.find_by(id: params[:id])
+    if @task&.update(completed: false)
+      render :mark_active
+    else
+      render json: { errors: @task.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ Rails.application.routes.draw do
   root 'homepage#index'
 
   # Add routes below this line
-
+  get 'task' => 'tasks#index'
   # Add routes below above line
+
+  post 'tasks' => 'tasks#create'
+
+  delete 'tasks/:id' => 'tasks#destroy'
+
+  put '/tasks/:id/mark_complete' => 'tasks#mark_complete'
+
+  put '/tasks/:id/mark_active'   => 'tasks#mark_active'
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
   get '*path' => 'homepage#index'
